805d67c739037091b00df5684990c4a777a43b98
Katherine Wren <kwren@outlook.com>
2019-04-22 19:47:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,43 @@
 # Write your code here.
+def dictionary
+  {"hello" => "hi", "to" => '2', "two" => '2', "too" => '2', "for" => '4', "four" => '4', "be" => 'b', "you" => 'u', "at" => "@", "and" => "&"}
+end
+
+def word_substituter(tweet)
+  dictionary
+  array = tweet.split(' ')
+  keywords = dictionary.keys
+    array.collect do |word|
+      keywords.each do |word2|
+        if word == word2
+          word = dictionary[word2]
+        end
+      end
+  end
+  array.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |string|
+    puts word_substituter(string)
+ end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else tweet
+end
+end
+
+shortened_tweet_truncator(tweet)
+  if tweet.length > 140 && word_substituter(tweet) > 140
+    newtweet = "#{tweet[0..140]}..."
+  elsif tweet.length > 140 && word_substituter(tweet) <= 140
+    newtweet = word_substituter(tweet)
+  else newtweet = tweet
+end
+newtweet
+end
+    
+  
